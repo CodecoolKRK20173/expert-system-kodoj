ff5bf5caec9e89f2539eaccc66a932575814fcde
kodoj <kodoj2@gmail.com>
2018-09-19 05:32:57 +0200

    abstract value and singleValue

--- expertSystem/src/main/java/containers/SingleValue.java
+++ expertSystem/src/main/java/containers/SingleValue.java
@@ -1,4 +1,12 @@
 package containers;
 
-public class SingleValue {
+import java.util.ArrayList;
+
+public class SingleValue extends Value {
+
+    public SingleValue(String parametr, boolean selectionType) {
+        this.inputPattern = new ArrayList<String>();
+        this.inputPattern.add(parametr);
+        this.selectionType = selectionType;
+    }
 }
--- expertSystem/src/main/java/containers/Value.java
+++ expertSystem/src/main/java/containers/Value.java
@@ -1,4 +1,17 @@
 package containers;
 
-public class Value {
+import java.util.List;
+
+public abstract class Value {
+
+    List<String> inputPattern;
+    boolean selectionType;
+
+    public List<String> getInputPattern() {
+        return this.inputPattern;
+    }
+
+    public boolean getSelectionType() {
+        return this.selectionType;
+    }
 }
