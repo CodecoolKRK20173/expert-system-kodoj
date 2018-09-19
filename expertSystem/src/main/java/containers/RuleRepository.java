86928286c2934eee02e622bcb776d8831533ab2b
kodoj <kodoj2@gmail.com>
2018-09-19 05:42:19 +0200

    ruleRepository

--- expertSystem/src/main/java/containers/RuleRepository.java
+++ expertSystem/src/main/java/containers/RuleRepository.java
@@ -1,4 +1,46 @@
 package containers;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class RuleRepository {
+
+    private List<Question> questions;
+
+    public RuleRepository() {
+        this.questions = new ArrayList<>();
+    }
+
+    public void addQuestion(Question question) {
+        this.questions.add(question);
+    }
+
+    public Iterator<Question> getIterator() {
+        return new QuestionIterator();
+    }
+
+
+    private class QuestionIterator implements Iterator<Question> {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            return index < questions.size();
+        }
+
+        @Override
+        public Question next() {
+            if (this.hasNext())
+                return questions.get(index++);
+
+            return null;
+        }
+
+        @Override
+        public void remove() {
+        }
+    }
+
 }
